0ff010c742f26f2f72bef84c5b3741cda3039f0c
Jackson-Augspurger <36752136+Jackson-Augspurger@users.noreply.github.com>
2018-12-09 23:50:07 -0600

    another build
    stuff

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class Block : MonoBehaviour {
 
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
+        FindObjectOfType<GameStatus>().AddToScore();
         Destroy(gameObject);
-        Debug.Log(collision.gameObject.name);
+        level.BlockDestroyed();
     }
 }
