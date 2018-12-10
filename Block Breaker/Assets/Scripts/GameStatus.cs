6766f1c82ca7883609fdef926f7e4f6a6935e52b
Jackson-Augspurger <36752136+Jackson-Augspurger@users.noreply.github.com>
2018-12-10 14:25:00 -0600

    bug fix

--- Block Breaker/Assets/Scripts/GameStatus.cs
+++ Block Breaker/Assets/Scripts/GameStatus.cs
@@ -7,9 +7,27 @@ public class GameStatus : MonoBehaviour {
 
     [Range(0.1f,10f)] [SerializeField] float gameSpeed = 1f;
 
-    [SerializeField] int pointsPerBlockDestroyed = 83;
-
+    [SerializeField] int pointsPerBlockDestroyed = 1;
     [SerializeField] int currentScore = 0;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if(gameStatusCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
 
 	// Update is called once per frame
 	void Update () {
@@ -19,5 +37,12 @@ public class GameStatus : MonoBehaviour {
     public void AddToScore()
     {
         currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
     }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+
 }
