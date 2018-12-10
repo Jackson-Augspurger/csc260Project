d2fdcd8ebcee3eb958d36778a7b744904e235c7a
Jackson-Augspurger <36752136+Jackson-Augspurger@users.noreply.github.com>
2018-12-09 21:47:05 -0600

    stuff

---
+++ Block Breaker/Assets/Scripts/Lose.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lose : MonoBehaviour {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        Debug.Log(collision.gameObject.name);
+    }
+}
